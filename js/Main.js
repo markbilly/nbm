8ef5f37a6db3e2f66b3d83a858895d8c8c001a54
Mark Richards <mark.billy@gmail.com>
2013-10-08 17:05:58 +0100

    Did my own engine

--- js/Main.js
+++ js/Main.js
@@ -1,31 +1,91 @@
-(function () {
+var c = document.getElementById("Game");
+var debug = document.getElementById("debug");
+var ctx = c.getContext("2d");
+var img = new Image();
+img.src = "NPI-2.png";
 
-    var game = new Phaser.Game(x(284), y(160), Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var player = {
+    image: null,
+    x: 0,
+    y: 0,
+    v: 0,
+    vd: 0,
+    ad: 10,
+    au: 0,
+    a: 10
+}
+var keys = [];
 
-    function preload() {
-        game.load.image('logo', 'logo.png');
+player.image = img;
+
+player.image.onload = function() {
+    Processor();
+}
+
+function Draw() {
+    ctx.drawImage(player.image,player.x,player.y);
+    
+    debug.innerHTML = "y = " + player.y +
+                        "<br>speed = " + player.vd +
+                        "<br>acceleration = " + player.a +
+                        "<br>a_up = " + player.au +
+                        "<br>a_down = " + player.ad;
+}
+
+function Processor() {
+    ctx.clearRect(0,0,500,500);
+    
+    //horizontal move
+    if (player.x > 500) {
+        player.x = 0;
     }
-
-    function create() {
-        // create player and configure
-        player = myGame.createSprite(0, 0, "entities");
-        player.drag.x = 900;
-        player.maxVelocity.x = 250;
-        player.animations.add('idle', ['player-idle-1.png'], 10, false, false);
-        player.animations.add('walk', ['player-walk-1-00.png', 'player-walk-1-01.png', 'player-walk-1-02.png', 'player-walk-1-03.png', 'player-walk-1-04.png', 'player-walk-1-05.png', 'player-walk-1-06.png', 'player-walk-1-07.png'], 10, true, false);
-        player.animations.play('idle');
+    else {
+        player.x += player.v;
     }
-
-    function update() {
+    //close
+    
+    //vertical move
+    player.a = player.ad - player.au;
+    player.vd += player.a;
+    player.y += player.vd;
+    
+    if (player.vd > 10) {
+        player.vd = 10;
     }
     
-    var player = Phaser.Sprite;
-
-})();
-
-function x(x) {
-    return x * 3;
+    if (player.y > 100) {
+        player.y = 100;
+        player.vd = 0;
+    }
+    //close
+    
+    Draw();
+    requestAnimationFrame(Processor);
 }
-function y(y) {
-    return y * 3;
+
+function keyPressed() {
+    // check keys
+    //put false conditions in
+    if (keys[38] === true) {
+        // up arrow
+        player.vd = -50;
+    }
+    if (keys[39] === true) {
+        // right arrow
+        player.v = 5;     
+    }          
+    if (keys[37] === true) {                 
+        // left arrow
+        player.v = -5;
+    }
 }
+
+document.body.addEventListener("keydown", function(e) {
+    keys[e.keyCode] = true;
+    keyPressed();
+});
+ 
+document.body.addEventListener("keyup", function(e) {
+    keys[e.keyCode] = false;
+    keyPressed();
+});
